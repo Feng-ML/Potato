872f8a0ac6dedc7b8b9153faa7a43561969200d6
fengminlin <1125335956@qq.com>
2023-08-17 00:18:35 +0800

    引入QFramework

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -4,54 +4,53 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
+using QFramework;
 
 public class MapManager : MonoBehaviour
 {
-    public GameObject bornAnimation;    //出生动画
+    public GameObject bornAnimation;        //出生动画
+    private List<EnemyPool> enemyPool;      //敌人对象池列表
+    public int bornNum = 10;                //生成敌人数量
+    public BindableProperty<float> gameTime = new BindableProperty<float>(10);  //游戏时间
+    public BindableProperty<float> bornTimer = new BindableProperty<float>(3);  //生成敌人间隔
 
-    private List<EnemyPool> enemyPool;    //敌人对象池列表
-
-    private float bornTimer;        //生成敌人间隔
-    public int bornNum = 10;        //生成敌人数量
-
-    public float gameTime = 30;
     public TMP_Text gameTimeText;
     public TMP_Text waveText;
 
     void Start()
     {
         enemyPool = PoolControl.Instance.enemyPool;
-        bornTimer = 3;
+
+        //监听关卡时间
+        gameTime.Register(newNum =>
+        {
+            gameTimeText.text = ((int)newNum).ToString();
+            if (newNum <= 0)
+            {
+                SceneManager.LoadScene("shop");
+            }
+        });
+        //监听敌人出生时间
+        bornTimer.Register(newNum =>
+        {
+            if (newNum > 3)
+            {
+                for (int i = 0; i < bornNum; i++)
+                {
+                    float x = UnityEngine.Random.Range(-16, 16);
+                    float y = UnityEngine.Random.Range(-7, 7);
+                    var enemyIndex = UnityEngine.Random.Range(0, 3);
+                    StartCoroutine(Born(2, new Vector2(x, y)));
+                }
+                bornTimer.Value = 0;
+            }
+        });
     }
 
     void Update()
     {
-
-        if (bornTimer > 3)
-        {
-            for (int i = 0; i < bornNum; i++)
-            {
-                float x = UnityEngine.Random.Range(-16, 16);
-                float y = UnityEngine.Random.Range(-7, 7);
-                var enemyIndex = UnityEngine.Random.Range(0, 3);
-                StartCoroutine(Born(2, new Vector2(x, y)));
-            }
-            bornTimer = 0;
-        }
-        else
-        {
-            bornTimer += Time.deltaTime;
-        }
-
-        if (gameTime <= 0)
-        {
-            SceneManager.LoadScene("shop");
-        }
-        else
-        {
-            gameTime -= Time.deltaTime;
-            gameTimeText.text = ((int)gameTime).ToString();
-        }
+        bornTimer.Value += Time.deltaTime;
+        gameTime.Value -= Time.deltaTime;
     }
 
     private IEnumerator Born(int enemyIndex, Vector2 position)
